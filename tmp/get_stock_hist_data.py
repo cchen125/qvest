8622fbcddcd1067dad81a395f9c3adf303830eef
cchen <chenchen0125@gmail.com>
2018-07-10 07:09:57 +0800

    update

--- tmp/get_stock_hist_data.py
+++ tmp/get_stock_hist_data.py
@@ -4,7 +4,7 @@ import tushare as ts
 import pickle
 
 # 获取股票列表
-stock_list = ts.get_stock_basics()
+stock_basics = ts.get_stock_basics()
 
 # 循环获取股票历史行情
 stock_hist_data = {}
@@ -12,13 +12,16 @@ start_dt = '2015-01-01'
 end_dt = '2018-06-30'
 
 counter = 0
-for code in stock_list.index.tolist():
+for code in stock_basics.index.tolist():
     counter += 1
     print(counter)
     stock_hist_data[code] = ts.get_hist_data(code, start=start_dt, end=end_dt)
 
-output = open('./stock_hist_data.pkl', 'wb')
-pickle.dump(stock_hist_data, file=output)
+with open('./stock_hist_data.pkl', 'wb') as fw:
+    pickle.dump(stock_hist_data, file=fw)
+
+with open('./stock_basics.pkl', 'wb') as fw:
+    pickle.dump(stock_basics, file=fw)
 
 # file_to_read = open('./stock_hist_data.pkl', 'rb')
 # res = pickle.load(file_to_read)
